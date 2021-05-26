74148a94335558cbc445a5bd004bf75afbba3cc8
jrdrenth <john.drenth@gmail.com>
2021-05-26 01:00:10 -0700

    Updated README.md

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -52,33 +52,7 @@ function Portfolio() {
       website: 'https://immense-brook-60528.herokuapp.com/',
       description: 'Workout Tracker'
     }    
-    
   ];
-
-  // function handleProjectClick (event) {
-  //   // Get the data-value of the clicked button
-  //   const btnType = event.target.attributes.getNamedItem("data-value").value;
-  //   // Clone this.state to the newState object
-  //   // We'll modify this object and use it to set our component's state
-  //   const newState = { ...this.state };
-
-  //   if (btnType === "pick") {
-  //     // Set newState.match to either true or false depending on whether or not the dog likes us (1/5 chance)
-  //     newState.match = 1 === Math.floor(Math.random() * 5) + 1;
-
-  //     // Set newState.matchCount equal to its current value or its current value + 1 depending on whether the dog likes us
-  //     newState.matchCount = newState.match
-  //       ? newState.matchCount + 1
-  //       : newState.matchCount;
-  //   } else {
-  //     // If we thumbs down'ed the dog, we haven't matched with it
-  //     newState.match = false;
-  //   }
-  //   // Replace our component's state with newState, load the next dog image
-  //   this.setState(newState);
-  //   this.loadNextDog();
-  // };
-
 
   return (
     // <div>
